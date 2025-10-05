56b6534ce43edc141d3002bff22814a5960d845b
sofiakonshina1-arch <sofia.konshina1@gmail.com>
2025-10-05 23:50:15 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,13 +4,15 @@
     {
         public static void Main()
         {
-            //White white = new White();
-            //Console.WriteLine(white.Task1(1.2));
-            //Console.WriteLine(white.Task2(2));
-            //Console.WriteLine(white.Task3(2.5, 1.89));
+            White white = new White();
+            //Console.WriteLine(white.Task1(-0.3));
+            //Console.WriteLine(white.Task2(-31));
+            //Console.WriteLine(white.Task3(2, 1));
             //Console.WriteLine(white.Task4(-2.5, 1.89));
             //Console.WriteLine(white.Task5(0.5));
             //Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            //Console.WriteLine(white.Task7(2));
+            //Console.WriteLine(white.Task8(10, 5, 2));
         }
     }
 }
